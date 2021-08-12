01e5c327c9e565314958d31f8cf4bf8c84760c84
rakeshragipani <56871705+rakeshragipani@users.noreply.github.com>
2021-08-12 16:13:15 +0530

    Update build.sh

--- bin/build.sh
+++ bin/build.sh
@@ -1,3 +1,3 @@
-docker build -t aravind43zz/crudapp_java:latest .
+docker build -t aravind43zz/crudapp_java:latest /var/lib/jenkins/workspace/crud-build-docker-image/.
 docker push aravind43zz/crudapp_java:latest
 #docker run -d -p 8081:8080 aravind43zz/crudapp_java:1.0
